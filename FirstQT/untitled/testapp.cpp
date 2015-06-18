7757a9c5ac6b568bcb592097f39d4478838d6c0e
fh666z <fh666z@gmail.com>
2015-06-19 00:07:03 +0300

    Experimenting with QT test app. Added .gitignore

--- FirstQT/untitled/testapp.cpp
+++ FirstQT/untitled/testapp.cpp
@@ -1,10 +1,24 @@
 #include "testapp.h"
 #include "ui_testapp.h"
 
-TestApp::TestApp(QWidget *parent) :
-    QMainWindow(parent),
-    ui(new Ui::TestApp)
+#include <QtGui>
+#include <QPushButton>
+#include <QTextEdit>
+#include <QVBoxLayout>
+
+TestApp::TestApp(QWidget *parent) : QMainWindow(parent), ui(new Ui::TestApp)
 {
+    QWidget *window = new QWidget();
+    QPushButton * b = new QPushButton("Shano", window);
+    QTextEdit * txt = new QTextEdit(window);
+
+    QVBoxLayout *ver_layout = new QVBoxLayout(window);
+    ver_layout->addWidget(txt);
+    ver_layout->addWidget(b);
+
+
+    window->show();
+
     ui->setupUi(this);
 }
 
