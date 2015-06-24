d9d30d3a08437bd340af712c00fb994cb51a3fca
Momchil Stanchev <fh666z@gmail.com>
2015-06-24 14:58:17 +0300

    Adjusted .gitignore. Part1 project modified to have slider and spinbox widget.

--- QT_Part1/demoui.cpp
+++ QT_Part1/demoui.cpp
@@ -2,15 +2,25 @@
 
 DemoUI::DemoUI(QApplication &app)
 {
+    // Create empty parent window
     m_main_window = new QWidget;
     m_main_window->setWindowTitle("My First QT App");
 
-    m_label = new QLabel("<h3><c>Demo UI</c></h3>", m_main_window);
+    // Widget creation and configuration
+    m_label = new QLabel("<h3><c>Age:</c></h3>", m_main_window);
     m_exit_btn = new QPushButton("Exit", m_main_window);
 
-    m_age_slider = new QSlider(m_main_window);
+    m_age_slider = new QSlider(Qt::Horizontal, m_main_window);
+    m_age_slider->setRange(18, 99);
     m_age_spinner = new QSpinBox(m_main_window);
+    m_age_spinner->setRange(18, 99);
 
+    QObject::connect(m_age_slider, SIGNAL(valueChanged(int)), m_age_spinner, SLOT(setValue(int)));
+    QObject::connect(m_age_spinner, SIGNAL(valueChanged(int)), m_age_slider, SLOT(setValue(int)));
+
+    m_age_slider->setValue(25);
+
+    // Layout stuff
     m_main_layout = new QVBoxLayout(m_main_window);
     m_age_layout = new QHBoxLayout(m_main_window);
 
